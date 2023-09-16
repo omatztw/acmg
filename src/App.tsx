7ecfb300e8ce0df852d8469b5e466010c1a10929
omatztw <33661309+omatztw@users.noreply.github.com>
2023-09-16 14:33:14 +0900

    first impl

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,154 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import { CsvUploadButton } from "./components/CsvUploadButton";
+import { csvParse } from "./util/file-helper";
+import { useState } from "react";
+import { CacheFlow } from "./components/CacheFlow";
+import { CSVData } from "./models/types";
+import { Grid, Paper, Tab, Tabs, TextField, Typography } from "@mui/material";
+import { CustomTabPanel } from "./components/CustomTabPannel";
+import { Preference } from "./components/Preference";
+import { usePersistState } from "./util/hooks";
+import { ExpenceResult } from "./components/ExpenceResult";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [csvData, setCsvData] = useState<CSVData[]>([]);
+  const [value, setValue] = useState(0);
+  const [partnerAccount, setPartnerAccount] = usePersistState<string[]>({
+    key: "partnerAccount",
+    initialValue: [],
+  });
+  const [expenceList, setExpenceList] = usePersistState<string[]>({
+    key: "expenceList",
+    initialValue: [],
+  });
+  const [expenceSubList, setExpenceSubList] = usePersistState<string[]>({
+    key: "expenceSubList",
+    initialValue: [],
+  });
+  const [rate, setRate] = usePersistState<number>({
+    key: "rate",
+    initialValue: 0.5,
+  });
+  const [partnerName, setPartnerName] = usePersistState<string>({
+    key: "partnerName",
+    initialValue: "パートナー",
+  });
+
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const handleSubmit = async (file: File) => {
+    try {
+      const csvCells = await csvParse(file, { header: false });
+      setCsvData(csvCells);
+      console.log(csvCells);
+    } catch (error) {
+      alert(error);
+    }
+  };
+  const handleRateChange = (e: any) => {
+    setRate(e.target.value);
+  };
+  const handlePartnerChange = (e: any) => {
+    setPartnerName(e.target.value);
+  };
+
+  const a11yProps = (index: number) => {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Container maxWidth={"xl"}>
+      <Box sx={{ mt: 3, mb: 2 }}>
+        <CsvUploadButton onSubmit={handleSubmit} />
+      </Box>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label='basic tabs example'
+        >
+          <Tab label='結果' {...a11yProps(0)} />
+          <Tab label='設定' {...a11yProps(1)} />
+          <Tab label='詳細' {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        <Box>
+          <ExpenceResult
+            data={csvData}
+            expence={expenceList}
+            expenceSub={expenceSubList}
+            partnerAccount={partnerAccount}
+            rate={rate}
+            partnerName={partnerName}
+          ></ExpenceResult>
+        </Box>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <Grid container>
+          <Grid item xs={3}>
+            <Typography variant='h5' sx={{ mt: 1 }}>
+              {"割合設定"}
+            </Typography>
+            <Box sx={{ mt: 2 }}>
+              <TextField
+                name='rate'
+                value={rate}
+                size='small'
+                onChange={(e) => handleRateChange(e)}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={3}>
+            <Typography variant='h5' sx={{ mt: 1 }}>
+              {"パートナー名"}
+            </Typography>
+            <Box sx={{ mt: 2 }}>
+              <TextField
+                name='name'
+                value={partnerName}
+                size='small'
+                onChange={(e) => handlePartnerChange(e)}
+              />
+            </Box>
+          </Grid>
+        </Grid>
 
-export default App
+        <Grid container>
+          <Grid item xs={3}>
+            <Preference
+              title={"経費大項目設定"}
+              list={expenceList}
+              setList={setExpenceList}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <Preference
+              title={"経費中項目設定"}
+              list={expenceSubList}
+              setList={setExpenceSubList}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <Preference
+              title={"パートナー金融機関設定"}
+              list={partnerAccount}
+              setList={setPartnerAccount}
+            />
+          </Grid>
+        </Grid>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        <Paper style={{ maxHeight: "calc(100vh - 130px)", overflow: "auto" }}>
+          <CacheFlow data={csvData} />
+        </Paper>
+      </CustomTabPanel>
+    </Container>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { CssBaseline } from "@mui/material";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
